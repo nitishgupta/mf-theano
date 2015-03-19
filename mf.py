7b4a360448513395e5702cc4146cb225c37cce04
Nitish Gupta <nitishgupta.iitk@gmail.com>
2015-03-19 22:42:54 +0530

    Updated cost function

--- mf.py
+++ mf.py
@@ -3,11 +3,14 @@ import theano.tensor as T
 from theano.tensor.shared_randomstreams import RandomStreams
 import theano
 import numpy as np
+from random import shuffle
+import sys
 
 def readData(filename):
+	ratings_read = 0
 	fi = open(filename, "r")
 	entities = defaultdict()
-	data = defaultdict()
+	data = []
 	for line in fi:
 		a = line.split()
 		if(len(a) > 1):
@@ -15,13 +18,14 @@ def readData(filename):
 			e2 = a[1].strip();
 			rate = float(a[2].strip())
 			if e1 not in entities.keys():
-				entities[e1] = len(entities) + 1
+				entities[e1] = len(entities)
 			if e2 not in entities.keys():
-				entities[e2] = len(entities) + 1
-			
-			if entities[e1] not in data.keys():
-				data[entities[e1]] = defaultdict()
-			data[entities[e1]][entities[e2]] = rate
+				entities[e2] = len(entities)
+			ratings_read = ratings_read + 1;
+			data.append([entities[e1], entities[e2], rate])	
+
+		if(ratings_read % 1000 == 0):
+			print ratings_read, " ratings read";		
 	return	entities, data
 
 def printData(data):
@@ -31,39 +35,75 @@ def printData(data):
 			print "\t", key1, data[key][key1]	
 
 
-def mf(entities, data, K, lr):
+def mae(data, predict):
+	for d in data:
+			mae = mae + abs(predict(d[0], d[1]) - d[2])
+	mae = mae / len(data)
+
+def mf(trdata, vdata, tdata, K, lr, reg, epoch):
 	srng = RandomStreams(seed=234)
 	rng = np.random
-	rate_t = T.fscalar('rate_true')
+	rate_t = T.dscalar('rate_true')
+	reg_con = T.dscalar('lambda')
 	e1 = T.iscalar('e1')
 	e2 = T.iscalar('e2')
 	alpha = theano.shared(rng.normal(loc=0.0, scale=0.01), name="alpha") 
 	b = theano.shared(rng.normal(loc=0.0, scale=0.01, size=(len(entities))), name="bias")
 	phi = theano.shared(rng.normal(loc=0.0, scale=0.01, size=(len(entities), K)), name="phi")
 	
-	b1, b2, phi1, phi2 = b[e1], b[e2], phi[e1], phi[e2]
-	prediction = alpha + b1 + b2 + T.dot(phi1, phi2)
-	cost = (prediction - rate_t)*(prediction - rate_t)
+	prediction = alpha + b[e1] + b[e2] + T.dot(phi[e1], phi[e2])
+	cost = (prediction - rate_t)*(prediction - rate_t) + reg_con*(alpha*alpha + b[e1]*b[e1] + b[e2]*b[e2] + T.dot(phi[e1],phi[e1]) + T.dot(phi[e2],phi[e2]) )
 
 	#ga, gb1, gb2, gphi1, gphi2 = T.grad(cost, [alpha, b1, b2, phi1, phi2])
 	ga, gb, gphi = T.grad(cost, [alpha, b, phi])
 	
 	predict = theano.function(inputs=[e1, e2], outputs = prediction)
-	c = theano.function(inputs=[e1, e2, rate_t], outputs=cost)
-	train = theano.function(inputs=[e1, e2, rate_t], outputs=[prediction, cost], updates= ( (b, b - lr*gb), (alpha, alpha - lr*ga), (phi, phi - lr*gphi)) )
+	c = theano.function(inputs=[e1, e2, rate_t, theano.Param(reg_con, default=reg)], outputs=cost)
+	train = theano.function(inputs=[e1, e2, rate_t, theano.Param(reg_con, default=reg)], outputs=[prediction, cost], updates= ( (b, b - lr*gb), (alpha, alpha - lr*ga), (phi, phi - lr*gphi)) )
 
 
-	print predict(1,2), c(1,2,4.0)
+	mae_val = 0.0
+	mae_test = 0.0
+	for i in range(0, epoch):
+		shuffle(trdata);
+		for d in trdata:
+			train(d[0], d[1], d[2])
 
-	print phi.get_value()[1]
-	print phi.get_value()[1]  - (predict(1,2) - 4.0)*phi.get_value()[2]*2*0.01
-	train(1,2,4.0);
-	print phi.get_value()[1]
+		#mae_val = mae(vdata, predict)
+		#mae_test = mae(tdata, predict)
+
+		for d in vdata:
+			mae_val = mae_val + pow(predict(d[0], d[1]) - d[2], 2)
+		mae_val = mae_val / len(vdata)
+		for d in tdata:
+			mae_test = mae_test + pow(predict(d[0], d[1]) - d[2], 2)
+		mae_test = mae_test / len(tdata)
+
+		print "Epoch : ", i, "validation mean sq. error : ", mae_val
+		print "Epoch : ", i, "testdataa  mean sq. error : ", mae_test
+		print 
+
 
 
 if __name__ == "__main__":
-	entities, data = readData("data.txt")		
-	print entities
-	print data
-	mf(entities, data, 5, 0.01)
+	entities, data = readData(sys.argv[1])
+	train_perc = 0.8
+	val_perc = 0.1
+	#print entities	
+	#mf(entities, data, 5, 0.01, 0.01)
+	shuffle(data);
+	trlen = int(len(data)*train_perc)
+	vallen = int(len(data)*val_perc)
+	trdata = data[0:trlen]
+	vdata = data[trlen:trlen+vallen]
+	testdata = data[trlen+vallen:-1]
+	
+	print len(data)
+	print len(trdata), len(vdata), len(testdata)
+
+	mf(trdata, vdata, testdata, K=5, lr=0.01, reg=0.01, epoch=50)
+
+
+
+
 			
